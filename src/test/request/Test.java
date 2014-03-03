e2b6f0032f7e9fde226ca958e31c695875b582f1
Zz-m <adjim1234@163.com>
2014-03-03 17:55:38 +0800

    nothing

---
+++ src/test/request/Test.java
@@ -0,0 +1,12 @@
+package test.request;
+
+public class Test {
+	public static void main(String[] args) {
+		int s = 2;
+		add(s);
+		System.out.println(s);
+	} 
+	static void add(int a) {
+		a++;
+	}
+}
--- src/test/request/TestAdd.java
+++ src/test/request/TestAdd.java
@@ -9,37 +9,42 @@ public class TestAdd {
 	 * @param args
 	 */
 	private static List<Item> list = new ArrayList<Item>();
+
 	public static void main(String[] args) {
 		TestAdd t = new TestAdd();
-		t.add(new Item("6",null,null,null));
-		t.add(new Item("5",null,null,null));
-		t.add(new Item("4",null,null,null));
-		t.add(new Item("3",null,null,null));
-		t.add(new Item("2",null,null,null));
-		t.add(new Item("1",null,null,null));
-		for(int i = 0; i < list.size(); i++){
+		t.add(new Item("6", null, null, null));
+		t.add(new Item("5", null, null, null));
+		t.add(new Item("4", null, null, null));
+		t.add(new Item("3", null, null, null));
+		t.add(new Item("2", null, null, null));
+		t.add(new Item("1", null, null, null));
+		for (int i = 0; i < list.size(); i++) {
 			System.out.println(list.get(i).getId());
 		}
 
 	}
+
 	public void add(Item item) {
-		if(item.getId() == null || item.getId() == "" || list.size() == 0) {
+		if (item.getId() == null || item.getId() == "" || list.size() == 0) {
 			list.add(item);
-		}else {
-			for(int i = 0; i < list.size(); i++) {
-				if(Integer.parseInt(list.get(i).getId()) > Integer.parseInt(item.getId()) || i == list.size()-1) {
-					if(i == list.size()-1 && Integer.parseInt(list.get(i).getId()) < Integer.parseInt(item.getId())) {
-						list.add(i+1, item);
+		} else {
+			for (int i = 0; i < list.size(); i++) {
+				if (Integer.parseInt(list.get(i).getId()) > Integer
+						.parseInt(item.getId()) || i == list.size() - 1) {
+					if (i == list.size() - 1
+							&& Integer.parseInt(list.get(i).getId()) < Integer
+									.parseInt(item.getId())) {
+						list.add(i + 1, item);
 						break;
 					}
 					list.add(i, item);
 					break;
-				}else {
+				} else {
 					continue;
 				}
 			}
 		}
-		
+
 	}
 
 }
